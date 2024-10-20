0d4511ab6f8116142f60a9deeec315e4afbdecdf
demagogen <roman.korennoy@gmail.com>
2024-10-20 22:27:51 +0300

    example done

--- test_binary_format.cpp
+++ test_binary_format.cpp
@@ -9,6 +9,29 @@ struct CMD
     unsigned int cmd : 5;
 };
 
+enum CMD_INFO
+{
+    IMM  = 0b00000001,
+    REG  = 0b00000010,
+    RAM  = 0b00000100
+};
+
+enum PROCESSOR_COMMANDS
+{
+    ERROR =  0,
+    HLT   =  1,
+    OUT   =  2,
+    PUSH  =  3,
+    POP   =  4,
+    ADD   =  5,
+    SUB   =  6,
+    MULT  =  7,
+    DIV   =  8,
+    PUSHR =  9,
+    POPR  = 10,
+    IN    = 11
+};
+
 char buffer[30] = {};
 // push == 00001;
 
@@ -16,7 +39,7 @@ int main()
 {
     FILE* file = fopen("new.asm", "wb");
     CMD Cmd = {};
-    const char* command = "push 50";
+    const char* command = "push [50]";
     int offset = 0;
     int reg    = 0;
     int value  = 0;
@@ -26,7 +49,7 @@ int main()
     sscanf(command, "%s%n", command_name, &offset);
     if (strcmp(command_name, "push") == 0)
     {
-        Cmd.cmd = 0b00001;
+        buffer[0] |= PUSH;
 
         char tmp[10] = {};
         int character_quantity = 0;
@@ -34,45 +57,48 @@ int main()
         {
             if (strchr(command + offset, '+'))
             {
-                Cmd.imm = 0b1;
-                Cmd.reg = 0b1;
-                Cmd.mem = 0b1;
+                buffer[0] |= REG | RAM | IMM;
                 sscanf(command + offset, " [+ %d]", &value);
                 sscanf(command + offset, " [r%cx]", &register_id);
             }
             else
             {
-                Cmd.mem = 0b1;
-                Cmd.reg = 0b1;
+                Cmd.mem = 1;
+                Cmd.reg = 1;
+                buffer[0] |= RAM | REG;
                 sscanf(command + offset, " [r%cx]", &register_id);
             }
-            if (register_id == 'a') register_id = 0;
-            if (register_id == 'b') register_id = 1;
-            if (register_id == 'c') register_id = 2;
-            if (register_id == 'd') register_id = 3;
         }
         else if (sscanf(command + offset, " [%d]", &value) == 1)
         {
-            Cmd.imm = 0b1;
-            Cmd.mem = 0b1;
+            Cmd.imm = 1;
+            Cmd.mem = 1;
+            buffer[0] |= IMM | RAM;
         }
         else if (sscanf(command + offset, " r%cx", &register_id))
         {
             Cmd.reg = 0b1;
+            buffer[0] |= REG;
             if (strchr(command + offset, '+'))
             {
                 if (sscanf(command + offset, " + %d", &value))
                 {
-                    Cmd.imm = 0b1;
+                    Cmd.imm = 1;
+                    buffer[0] |= IMM;
                 }
             }
         }
         else if (sscanf(command + offset, " %d", &value))
         {
-            Cmd.imm = 0b1;
+            Cmd.imm = 1;
+            buffer[0] |= IMM;
         }
+        if (register_id == 'a') register_id = 0;
+        if (register_id == 'b') register_id = 1;
+        if (register_id == 'c') register_id = 2;
+        if (register_id == 'd') register_id = 3;
 
-        buffer[0] |= Cmd.mem | Cmd.reg | Cmd.imm | Cmd.cmd;
+
     }
     printf("cmd            %x\n", Cmd.cmd);
     printf("mem            %x\n", Cmd.mem);
@@ -81,8 +107,13 @@ int main()
     printf("register_value %x\n", register_id);
     printf("value          %x\n", value);
 
-    buffer[0] |= Cmd.cmd | Cmd.mem | Cmd.reg | Cmd.imm;
-    printf("%b\n", buffer[0]);
+    char check  = 0;
+    char check2 = 0;
+    check  |= Cmd.cmd;
+    check2 |= Cmd.imm;
+    printf("check          %x\n", check);
+    printf("check2         %x\n", check2);
+    printf("cmd in buffer  %x\n", buffer[0]);
     *(int *)(buffer + 1) = value;
 
     fwrite(buffer, sizeof(char), 30, file);
